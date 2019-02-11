89dbad800a95e20ed9b6e759e1b6157d19088e86
Prisma <withinfinitecourage@gmail.com>
2019-02-10 17:25:33 -0800

    Add files via upload

---
+++ ex3/predictOneVsAll.m
@@ -0,0 +1,47 @@
+function p = predictOneVsAll(all_theta, X)
+%PREDICT Predict the label for a trained one-vs-all classifier. The labels 
+%are in the range 1..K, where K = size(all_theta, 1). 
+%  p = PREDICTONEVSALL(all_theta, X) will return a vector of predictions
+%  for each example in the matrix X. Note that X contains the examples in
+%  rows. all_theta is a matrix where the i-th row is a trained logistic
+%  regression theta vector for the i-th class. You should set p to a vector
+%  of values from 1..K (e.g., p = [1; 3; 1; 2] predicts classes 1, 3, 1, 2
+%  for 4 examples) 
+
+m = size(X, 1);
+num_labels = size(all_theta, 1);
+
+% You need to return the following variables correctly 
+p = zeros(size(X, 1), 1);
+
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Complete the following code to make predictions using
+%               your learned logistic regression parameters (one-vs-all).
+%               You should set p to a vector of predictions (from 1 to
+%               num_labels).
+temp = sigmoid( X*all_theta'); % 5000 rows, each has 10 possibility
+
+% transpost temp, so 10 rows, 5000 colums
+[probability index] = max(temp'); % get in index of the max of every colum
+p = index'; % since index is a row vector, it need to be transposed.
+
+% Hint: This code can be done all vectorized using the max function.
+%       In particular, the max function can also return the index of the 
+%       max element, for more information see 'help max'. If your examples 
+%       are in rows, then, you can use max(A, [], 2) to obtain the max 
+%       for each row.
+%       
+
+
+
+
+
+
+
+% =========================================================================
+
+
+end
